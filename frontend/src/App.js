c401b3f0e3f6aa2e55f187883844a0bc81c9ed86
meghashree <meghapoojary385@gmail.com>
2023-10-14 23:01:03 +0530

    commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Shopzyy</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+    
+     
+  );
+}
+
+export default App;
